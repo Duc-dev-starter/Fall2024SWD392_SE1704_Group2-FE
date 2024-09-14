76c159ada731c465c3c882a37322b82f476970e3
Duc-dev-starter <leminhduck@gmail.com>
2024-09-14 11:46:27 +0700

    responsive for mobile and fix footer ui

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import { HomePage } from "@/pages";
+import { HomePage, AboutPage } from "@/pages";
 import { PATHS } from "@/consts";
 
 const AppRouter: React.FC = () => {
     return (
         <Routes>
             <Route path={PATHS.HOME} element={<HomePage />} />
+            <Route path={PATHS.ABOUT} element={<AboutPage />} />
         </Routes>
     )
 }
